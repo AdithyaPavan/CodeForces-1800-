ac4cf0075eab18ef7f9e9ae3100eb979d67be252
AdithyaPavan <136152474+AdithyaPavan@users.noreply.github.com>
2025-11-22 20:18:13 +0530

    Add files via upload

---
+++ ArrayColoring.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ArrayColoring {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0) {
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            int odd=0;
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+                if(arr[i]%2==1)odd++;
+            }
+            if(odd%2==1)System.out.println("NO");
+            else System.out.println("YES");
+        }
+    }
+}
